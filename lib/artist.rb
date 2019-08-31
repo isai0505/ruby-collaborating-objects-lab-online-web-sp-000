de3b56fac2d736fbb01609465e51bbda150a56b0
Isai Bismark Cortez <isai0505@gmail.com>
2019-08-31 04:03:27 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -61,6 +61,10 @@ class Artist
     @@all
   end
 
+  def songs
+    @songs
+  end
+
   def self.find_or_create_by_name(name)
     self.all.detect {|artist| artist.name == name} || Artist.new(name)
   end
