3f974782bbecdea39e5a4242466b9d03c47bbebe
Isai Bismark Cortez <isai0505@gmail.com>
2019-08-31 04:18:52 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -25,8 +25,14 @@ class Song
 
   attr_accessor :name, :artist
 
+  @@all = []
+
   def initialize(name)
     @name = name
+  end
+
+  def save
+    @@all << self
   end
 
   def self.new_by_filename(filename)
