196f8e39be2ebf5cd410ba68d3e7677d73045f39
Isai Bismark Cortez <isai0505@gmail.com>
2019-08-31 03:11:57 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -25,7 +25,7 @@ class MP3Importer
 
   def files
     @files = Dir.entries(@path)
-    @files.delete_if {|file| file == ".." || file == ".."}
+    @files.delete_if {|file| file == "." || file == ".."}
   end
 
 end
