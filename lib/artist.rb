e3ae56394b546485fd7fe1582fae21cb9f525992
Isai Bismark Cortez <isai0505@gmail.com>
2019-08-31 02:53:08 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -26,16 +26,11 @@ class Artist
 
 
   def self.find_or_create_by_name(name)
-    artist = @@all.find {|artist| artist.name == name}
-    if artist == nil
-      artist = Artist.new(name)
-      artist.save
-  end
-    @@all.last
+    self.all.detect {|artist| artist.name == name} || Artist.new(name)
   end
 
-   def print_songs
-    @songs.each{|song| puts "#{song}"}
+  def print_songs
+    @songs.each{|song| puts song.name}
   end
 
 
