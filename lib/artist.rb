5a3abb5ee460bc61a801d611e2f136e0d0dd798e
Isai Bismark Cortez <isai0505@gmail.com>
2019-08-31 03:18:57 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -41,8 +41,15 @@ class Artist
 
   attr_accessor :name
 
+  @@all = []
+
   def initialize(name)
     @name = name
+    @song = []
   end
-  
+
+  def add_song(song)
+    @songs << song 
+  end
+
 end
