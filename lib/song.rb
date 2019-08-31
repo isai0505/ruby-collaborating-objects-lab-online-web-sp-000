8130cb3ce2da6e38cd5a5fed4fa126ece0af75dc
Isai Bismark Cortez <isai0505@gmail.com>
2019-08-31 04:21:18 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -41,7 +41,7 @@ class Song
   end
 
   def self.new_by_filename(filename)
-    splitname = filename.split(" - ")
+    split_name = filename.split(" - ")
     self.new(split_name[1])
     artist = Artist.find_or_create_by_name(split_name[0])
     song.artist = artist
