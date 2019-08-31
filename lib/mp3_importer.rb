8831c8bb1c5cb94fb01185fa59f10ae07eee47ab
Isai Bismark Cortez <isai0505@gmail.com>
2019-08-31 03:10:25 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -1,16 +1,31 @@
+# class MP3Importer
+#   attr_accessor :path, :file_array
+#
+#   def initialize(filepath)
+#     @path = filepath
+#   end
+#
+#   def files
+#     @file_array = Dir.entries(@path).select{|file| file.include?(".mp3")}
+#   end
+#
+#   def import
+#     self.files
+#     @file_array.each{|song| Song.new_by_filename(song)}
+#   end
+# end
+
 class MP3Importer
-  attr_accessor :path, :file_array
+
+  attr_accessor : :path
 
   def initialize(filepath)
     @path = filepath
   end
 
   def files
-    @file_array = Dir.entries(@path).select{|file| file.include?(".mp3")}
+    @files = Dir.entries(@path)
+    @files.delete_if {|file| file == ".."}
   end
 
-  def import
-    self.files
-    @file_array.each{|song| Song.new_by_filename(song)}
-  end
 end
