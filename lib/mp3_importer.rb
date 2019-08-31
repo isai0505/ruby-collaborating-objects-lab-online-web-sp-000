16885a1f2c71c62359e67b60d81762e469d90554
Isai Bismark Cortez <isai0505@gmail.com>
2019-08-31 04:26:50 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -31,7 +31,6 @@ class MP3Importer
   def import
     self.files.each do |file|
       song = Song.new_by_filename(file)
-      Artist.all << song.artist unless Artist.all.include?(song.artist)
     end
   end
 
